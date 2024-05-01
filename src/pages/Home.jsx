3fb651bcfef3c0cdba1c46ff9abace81f3336b35
sota <soryu1111@gmail.com>
2024-05-01 19:51:32 +0900

    use api in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,29 @@
 import Layout from "@/components/Layout";
 import Slide from "@/components/Slide";
 import TopNote from "@/components/TopNote";
+import { WP_API_BASE_URL } from "@/settings/wordPress";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Home = () => {
+  const { data } = useQuery({
+    queryKey: ["home"],
+    queryFn: async () => {
+      const response = await axios.get(
+        `${WP_API_BASE_URL}/wp-json/wp/v2/pages/2`
+      );
+      return response.data;
+    },
+  });
+
   return (
     <Layout>
       <Slide />
-      <section className="top-introduction">
+      {/* <section className="top-introduction">
         <div className="top-introduction-container">
           <div className="top-introduction-flex-item1">
-            <h2>
-              信頼性試験を支える
-              <br />
-              エタックの環境試験技術
-            </h2>
+            <h2>信頼性試験を支える</h2>
+            <h2>エタックの環境試験技術</h2>
           </div>
           <div className="top-introduction-flex-item2">
             <p className="no-wrap">
@@ -34,7 +44,11 @@ const Home = () => {
             </p>
           </div>
         </div>
-      </section>
+      </section> */}
+      <section
+        className="top-introduction"
+        dangerouslySetInnerHTML={{ __html: data?.content?.rendered }}
+      />
       <TopNote />
     </Layout>
   );
